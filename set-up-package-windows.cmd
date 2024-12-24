d81cb442f7ecd22303c4e41985e696192a415e18
MinhTD <61413434+Leomin07@users.noreply.github.com>
2024-12-24 10:13:08 +0700

    Update set-up-package-windows.cmd

--- set-up-package-windows.cmd
+++ set-up-package-windows.cmd
@@ -9,18 +9,29 @@ cls
 echo 1.install_choco
 echo 2.update_choco
 echo 3.install_wsl
-echo 4.install_normal
-echo 5.Quit
+echo 4.windows
+echo 5.dev
+echo 6.game
+echo 7.refreshenv
+echo 8.Quit
 echo.
 
-choice /C 123456 /M "Enter your choice:"
+choice /C 1234567 /M "Enter your choice:"
 
 :: Note - list ERRORLEVELS in decreasing order
-IF ERRORLEVEL 5 GOTO exits
-IF ERRORLEVEL 4 GOTO install_normal
+IF ERRORLEVEL 8 GOTO exits
+IF ERRORLEVEL 6 GOTO game
+IF ERRORLEVEL 7 GOTO refreshenv
+IF ERRORLEVEL 5 GOTO dev
+IF ERRORLEVEL 4 GOTO windows
 IF ERRORLEVEL 3 GOTO install_wsl
 IF ERRORLEVEL 2 GOTO update_choco
 IF ERRORLEVEL 1 GOTO install_choco
+
+:refreshenv
+refreshenv
+pause
+goto begin
 
 :install_choco
 REM ----------------------------------------------------------------------------
@@ -40,34 +51,57 @@ echo Done!
 pause
 goto begin
 
+REM ----------------------------------------------------------------------------
+REM                                  game
+REM ----------------------------------------------------------------------------
 
-:install_normal
-REM ----------------------------------------------------------------------------
-REM                                  Normal
-REM ----------------------------------------------------------------------------
+:game
 choco install jetbrainsmono -y
-choco install nodejs-lts -y
-choco install yarn -y
-choco install python -y
-choco install git -y
+choco install googlechrome -y
 choco install vscode -y
-choco install 7zip -y
-choco install winrar -y
-choco install brave -y
-choco install potplayer -y
-choco install powertoys -y
-choco install microsoft-windows-terminal -y
-choco install internet-download-manager -y
-choco install sharpkeys -y
 choco install chocolateygui -y
 choco install telegram -y
 choco install everything -y
-choco install flow-launcher -y
+choco install warp -y
+choco install powertoys -y
+choco install internet-download-manager -y
+choco install microsoft-windows-terminal -y
+echo Done!
+pause
+goto begin
+REM ----------------------------------------------------------------------------
+REM                                  Normal
+REM ----------------------------------------------------------------------------
+
+:dev
+choco install jetbrainsmono -y
+choco install nodejs-lts -y
+choco install yarn -y
 choco install docker-desktop -y
 choco install dbeaver -y
 choco install mongodb-compass -y
 choco install postman -y
+echo Done!
+pause
+goto begin
+
+:windows
+choco install python -y
+choco install git -y
+choco install microsoft-windows-terminal -y
+choco install alacritty -y
+choco install vscode -y
+
+choco install powertoys -y
+choco install internet-download-manager -y
+
+choco install chocolateygui -y
+choco install telegram -y
+choco install everything -y
+choco install flow-launcher -y
 choco install mpv -y
+choco install warp -y
+
 echo Done!
 pause
 goto begin
